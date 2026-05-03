4bc8772284d0c3efa508acc753af03b7eb2c25a9
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2026-05-03 10:54:29 +0700

    Update: PWA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
 export default function Footer() {
-    
+
     const handleClick = (e, id) => {
-        e.preventDefault(); 
+        e.preventDefault();
         const element = document.getElementById(id);
         element.scrollIntoView({ behavior: 'smooth' });
     }
 
     return (
         <>
-        <footer className="footer footer-center p-12 bg-nb-black text-white border-t-8 border-black">
-          <aside className="items-center grid-flow-col">
-            <p className="font-black uppercase tracking-widest text-lg">
-                <a href="#" onClick={(e) => handleClick(e, 'home')} className="hover:text-nb-yellow transition-colors">
-                    © Rafly Anggara Putra. Built with <span className="text-nb-pink">Logic</span>.
-                </a>
-            </p>
-          </aside> 
-        </footer>
+            <footer className="footer footer-center p-12 bg-nb-black text-white border-t-8 border-black">
+                <aside className="items-center grid-flow-col">
+                    <p className="font-black uppercase tracking-widest text-lg">
+                        <a href="#" onClick={(e) => handleClick(e, 'home')} className="hover:text-nb-yellow transition-colors">
+                            © Rafly Anggara Putra. Build and <span className="text-nb-pink">Learning!</span>.
+                        </a>
+                    </p>
+                </aside>
+            </footer>
         </>
     )
 }
